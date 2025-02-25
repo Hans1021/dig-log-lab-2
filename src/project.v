85594b7fc8717751d7cb36e6d1500013718ecea3
Hans1021 <yhh051021@gmail.com>
2025-02-24 23:20:42 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -15,44 +15,26 @@ module tt_um_project (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-    
-module priority_encoder_16to8 (en, In, C);
-    input en;             
-    input [15:0] In;      // 16-bit input
-    output reg [7:0] C;   // 8-bit output
 
-    always @(en, In) 
-      begin
-        if (en == 1) 
-          begin
-            if (In[15] == 1) C = 8'b0000_1111;
-            else if (In[14] == 1) C = 8'b0000_1110;
-            else if (In[13] == 1) C = 8'b0000_1101;
-            else if (In[12] == 1) C = 8'b0000_1100;
-            else if (In[11] == 1) C = 8'b0000_1011;
-            else if (In[10] == 1) C = 8'b0000_1010;
-            else if (In[9]  == 1) C = 8'b0000_1001;
-            else if (In[8]  == 1) C = 8'b0000_1000;
-            else if (In[7]  == 1) C = 8'b0000_0111;
-            else if (In[6]  == 1) C = 8'b0000_0110;
-            else if (In[5]  == 1) C = 8'b0000_0101;
-            else if (In[4]  == 1) C = 8'b0000_0100;
-            else if (In[3]  == 1) C = 8'b0000_0011;
-            else if (In[2]  == 1) C = 8'b0000_0010;
-            else if (In[1]  == 1) C = 8'b0000_0001;
-            else if (In[0]  == 1) C = 8'b0000_0000;
-            else C = 8'b1111_0000; // Special case: all zeros
-        end
-  
-        else C = 8'bzzzz_zzzz;
+    always @(*) begin
+        if (ui_in[7] == 1) uo_out = 8'b0000_1111;
+        else if (ui_in[6] == 1) uo_out = 8'b0000_1110;
+        else if (ui_in[5] == 1) uo_out = 8'b0000_1101;
+        else if (ui_in[4] == 1) uo_out = 8'b0000_1100;
+        else if (ui_in[3] == 1) uo_out = 8'b0000_1011;
+        else if (ui_in[2] == 1) uo_out = 8'b0000_1010;
+        else if (ui_in[1]  == 1) uo_out = 8'b0000_1001;
+        else if (ui_in[0]  == 1) uo_out = 8'b0000_1000;
+        else if (uio_in[7]  == 1) uo_out = 8'b0000_0111;
+        else if (uio_in[6]  == 1) uo_out = 8'b0000_0110;
+        else if (uio_in[5]  == 1) uo_out = 8'b0000_0101;
+        else if (uio_in[4]  == 1) uo_out = 8'b0000_0100;
+        else if (uio_in[3]  == 1) uo_out = 8'b0000_0011;
+        else if (uio_in[2]  == 1) uo_out = 8'b0000_0010;
+        else if (uio_in[1]  == 1) uo_out = 8'b0000_0001;
+        else if (uio_in[0]  == 1) uo_out = 8'b0000_0000;
+        else uo_out = 8'b1111_0000; // Special case: all zeros
     end
-endmodule
-
-priority_encoder_16to8 encoder_inst (
-    .en(1),
-    .In({ui_in, uio_in}),
-    .C(uo_out)
-);
   assign uio_out = 0;
   assign uio_oe  = 0;
 
