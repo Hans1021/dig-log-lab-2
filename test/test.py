d60b860bb638fbeb785a94d0a9ed51da57117423
Hans1021 <yhh051021@gmail.com>
2025-02-25 23:09:46 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -47,12 +47,12 @@ async def test_project(dut):
 
     assert dut.uo_out.value == 0b11110000
 
-    dut.ui_in.value = 0b00101010
-    dut.uio_in.value = 0b11110001
+    dut.ui_in.value = 0b00000010
+    dut.uio_in.value = 0b01010011
 
     await ClockCycles(dut.clk, 1)
 
-    assert dut.uo_out.value == 13
+    assert dut.uo_out.value == 9
 
     dut.ui_in.value = 0b00000000
     dut.uio_in.value = 0b00101011
