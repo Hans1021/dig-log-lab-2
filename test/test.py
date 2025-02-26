800a3c9f13943525ff0373614bc5f5094728d036
Hans1021 <yhh051021@gmail.com>
2025-02-25 23:01:19 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -26,44 +26,44 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     
-    dut.ui_in.value = 0b00101010
-    dut.uio_in.value = 0b11110001
+    dut.ui_in.value = 235
+    dut.uio_in.value = 50
 
     await ClockCycles(dut.clk, 1)
 
-    assert dut.uo_out.value == 13
+    assert dut.uo_out.value == 185
 
-    dut.ui_in.value = 0b00000000
-    dut.uio_in.value = 0b00000001
+    dut.ui_in.value = 5
+    dut.uio_in.value = 5
 
     await ClockCycles(dut.clk, 1)
 
     assert dut.uo_out.value == 0
     
-    dut.ui_in.value = 0b00000000
-    dut.uio_in.value = 0b00000000
+    dut.ui_in.value = 100
+    dut.uio_in.value = 115
 
     await ClockCycles(dut.clk, 1)
 
-    assert dut.uo_out.value == 0b11110000
+    assert dut.uo_out.value == 15
     
-    dut.ui_in.value = 0b00101010
-    dut.uio_in.value = 0b11110001
+    dut.ui_in.value = 55
+    dut.uio_in.value = 13
 
     await ClockCycles(dut.clk, 1)
 
-    assert dut.uo_out.value == 13
+    assert dut.uo_out.value == 42
     
-    dut.ui_in.value = 0b00011001
-    dut.uio_in.value = 0b10101011
+    dut.ui_in.value = 45
+    dut.uio_in.value = 211
 
     await ClockCycles(dut.clk, 1)
 
-    assert dut.uo_out.value == 12
+    assert dut.uo_out.value == 166
 
-    dut.ui_in.value = 0b00001111
-    dut.uio_in.value = 0b10101111
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
 
     await ClockCycles(dut.clk, 1)
 
-    assert dut.uo_out.value == 11
+    assert dut.uo_out.value == 0
